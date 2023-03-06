f495a1cfa6e07df33cc8ca257e90f67cd7d433fa
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-03-06 14:59:06 +1000

    download books from all pages.

--- parse_tululu.py
+++ parse_tululu.py
@@ -1,5 +1,7 @@
 # import os
+import os.path
 from time import sleep
+import json
 import sys
 import requests
 from pathlib import Path
@@ -7,6 +9,25 @@ from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import argparse
+
+
+def get_list_nf_books():
+    lst = []
+    for j in range(1, 5):
+        url = f'https://tululu.org/l55/{j}/'
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        x = soup.find_all('table', class_='d_book')
+        for i in x:
+            y = str(i.find('a')).split()
+            link = urljoin('https://tululu.org/', y[1][7:-1])
+            lst.append(link)
+
+    return lst
 
 
 def get_book_page(book_id):
@@ -36,26 +57,27 @@ def parse_book_page(response):
     soup = BeautifulSoup(response.text, 'lxml')
 
     book_title_tag = soup.find('h1')
-    book_title, author = book_title_tag.text.split('::')
+    book_title, book_author = book_title_tag.text.split('::   ')
     book_title = book_title.strip()
 
     cover_image_tag = soup.find('div', class_='bookimage').find('img')
     cover_image_url = urljoin(response.url, cover_image_tag['src'])
 
     book_comments_tag = soup.find_all('div', class_='texts')
-    book_comments = '\n'.join([tag.find('span').text for tag in book_comments_tag])
+    # book_comments = '\n'.join([tag.find('span').text for tag in book_comments_tag])
+    book_comments = [tag.find('span').text for tag in book_comments_tag]
 
     book_genre_tag = soup.find('span', class_='d_book').find_all('a')
     book_genres = [tag.text for tag in book_genre_tag]
 
-    return book_title, cover_image_url, book_comments, book_genres
+    return book_title, book_author, cover_image_url, book_comments, book_genres
 
 
 def download_book_txt(book_id, filename, folder='books/'):
     """Функция для скачивания текстовых файлов.
         Args:
             # url (str): Cсылка на текст, который хочется скачать.
-            book_id (int): Номер книги, которую хочется скачать.
+            book_id (str): Номер книги, которую хочется скачать.
             filename (str): Имя файла, с которым сохранять.
             folder (str): Папка, куда сохранять. По умолчанию "books/"
         Returns:
@@ -74,7 +96,7 @@ def download_book_txt(book_id, filename, folder='books/'):
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
-    return filepath
+    return str(filepath)
 
 
 def download_book_cover(url, filename, folder='images/'):
@@ -97,36 +119,57 @@ def download_book_cover(url, filename, folder='images/'):
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
-    return filepath
+    return str(filepath)
 
 
 def main():
     connection_waiting_seconds = 10
 
-    parser = create_parser()
-    if len(sys.argv) < 3:
-        parser.print_help()
-    args = parser.parse_args()
+    nf_books = get_list_nf_books()
+    nf_books_reduced = [b.split('/')[-2][1:] for b in nf_books]
 
-    for book_id in range(args.start_index, args.end_index + 1):
+    print(nf_books_reduced)
+
+    # parser = create_parser()
+    # if len(sys.argv) < 3:
+    #     parser.print_help()
+    # args = parser.parse_args()
+    #
+    # for book_id in range(args.start_index, args.end_index + 1):
+
+    books_annotations = []
+
+    for book_id in nf_books_reduced:
         is_connected = True
         number_of_tries = 5
 
         while number_of_tries > 0:
             try:
-                response = get_book_page(book_id)
-                book_title, cover_image_url, book_comments, book_genres = parse_book_page(response)
 
-                print(f'!!! !!! !!!{book_title} {cover_image_url} {book_comments} {book_genres}')
+                response = get_book_page(book_id)
+                book_title, book_author, cover_image_url, book_comments, book_genres = parse_book_page(response)
 
                 txt_name = f'{book_id}. {book_title}'
 
-                print(download_book_txt(book_id, txt_name))
-                print(download_book_cover(cover_image_url, str(book_id)))
+                print(text_path := download_book_txt(book_id, txt_name))
+                print(cover_path := download_book_cover(cover_image_url, cover_image_url.split('/')[-1]))
                 print()
-                print(book_genres) if book_genres else print('There is no genres for this book!')
-                print(book_comments) if book_comments else print('There is no comments for this book')
+                print(genres := book_genres if book_genres else 'There is no genres for this book!')
+                print(comments := book_comments if book_comments else 'There is no comments for this book')
                 print()
+
+                book_describe = {
+                    'title': book_title,
+                    'author': book_author,
+                    'img_src': cover_path,
+                    'book_path': text_path,
+                    'comments': comments,
+                    'genres': genres,
+                }
+
+                if os.path.exists(text_path):
+                    books_annotations.append(book_describe)
+
                 break
             except requests.ConnectionError:
                 if is_connected:
@@ -144,6 +187,10 @@ def main():
                 print(f'Book "{txt_name}" loading problem, check the data.')
             number_of_tries -= 1
 
+    with open('./info.json', 'w', encoding='utf-8') as file:
+        json.dump(books_annotations, file, indent=True, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     main()
+    # print(*get_list_nf_books(), sep='\n')
